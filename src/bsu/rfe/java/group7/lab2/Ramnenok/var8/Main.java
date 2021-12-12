18dcbfccc05d936cd18baa73ea7a4fc3c1f2f481
Nikita Ramnjonok <92184855+NikitaRamnionak@users.noreply.github.com>
2021-12-12 20:18:43 +0300

    Update Main.java

--- src/bsu/rfe/java/group7/lab2/Ramnenok/var8/Main.java
+++ src/bsu/rfe/java/group7/lab2/Ramnenok/var8/Main.java
@@ -36,4 +36,80 @@ public class Main extends JFrame {
 
     // Флаг, указывающий на загруженность данных графика
     private boolean fileLoaded = false;
+    public Main() {
+// Вызов конструктора предка Frame
+        super("Построение графиков функций на основе заранее подготовленных файлов");
+
+// Установка размеров окна
+        setSize(WIDTH, HEIGHT);
+        Toolkit kit = Toolkit.getDefaultToolkit();
+
+// Отцентрировать окно приложения на экране
+        setLocation((kit.getScreenSize().width - WIDTH) / 2, (kit.getScreenSize().height - HEIGHT) / 2);
+
+// Развѐртывание окна на весь экран
+        setExtendedState(MAXIMIZED_BOTH);
+
+// Создать и установить полосу меню
+        JMenuBar menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+
+// Добавить пункт меню "Файл"
+        JMenu fileMenu = new JMenu("Файл");
+        menuBar.add(fileMenu);
+
+// Создать действие по открытию файла
+        Action openGraphicsAction = new AbstractAction("Открыть файл с графиком") {
+            public void actionPerformed(ActionEvent event) {
+                if (fileChooser == null) {
+                    fileChooser = new JFileChooser();
+                    fileChooser.setCurrentDirectory(new File("."));
+                }
+                if (fileChooser.showOpenDialog(Main.this) == JFileChooser.APPROVE_OPTION)
+                    openGraphics(fileChooser.getSelectedFile());
+            }
+        };
+        protected void openGraphics (File selectedFile){
+            try {
+// Шаг 1 - Открыть поток чтения данных, связанный с входным файловым потоком
+                DataInputStream in = new DataInputStream(new FileInputStream(selectedFile));
+/* Шаг 2 - Зная объѐм данных в потоке ввода можно вычислить,
+* сколько памяти нужно зарезервировать в массиве:
+* Всего байт в потоке - in.available() байт;
+* Размер одного числа Double - Double.SIZE бит, или
+Double.SIZE/8 байт;
+* Так как числа записываются парами, то число пар меньше в
+2 раза
+*/
+                Double[][] graphicsData = new Double[in.available() / (Double.SIZE / 8) / 2][];
+// Шаг 3 - Цикл чтения данных (пока в потоке есть данные)
+                int i = 0;
+                while (in.available() > 0) {
+// Первой из потока читается координата точки X
+                    Double x = in.readDouble();
+// Затем - значение графика Y в точке X
+                    Double y = in.readDouble();
+// Прочитанная пара координат добавляется в массив
+                    graphicsData[i++] = new Double[]{x, y};
+                }
+// Шаг 4 - Проверка, имеется ли в списке в результате чтения хотя бы одна пара координат
+                if (graphicsData != null && graphicsData.length > 0) {
+// Да - установить флаг загруженности данных
+                    fileLoaded = true;
+// Вызывать метод отображения графика
+                    display.showGraphics(graphicsData);
+                }
+// Шаг 5 - Закрыть входной поток
+                in.close();
+            } catch (FileNotFoundException ex) {
+// В случае исключительной ситуации типа "Файл не найден" показать сообщение об ошибке
+                JOptionPane.showMessageDialog(Main.this, "Указанный файл не найден", "Ошибка загрузки данных", JOptionPane.WARNING_MESSAGE);
+                return;
+            } catch (IOException ex) {
+// В случае ошибки ввода из файлового потока показать сообщение об ошибке
+                JOptionPane.showMessageDialog(Main.this, "Ошибка чтения координат точек из файла", "Ошибка загрузки данных", JOptionPane.WARNING_MESSAGE);
+                return;
+            }
+        }
+    }
 }
