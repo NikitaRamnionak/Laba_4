8efe25fe31737a46b91fdcf5f525d6ab2c44ed43
Nikita Ramnjonok <92184855+NikitaRamnionak@users.noreply.github.com>
2021-12-12 20:16:55 +0300

    Update Main.java

--- src/bsu/rfe/java/group7/lab2/Ramnenok/var8/Main.java
+++ src/bsu/rfe/java/group7/lab2/Ramnenok/var8/Main.java
@@ -1,8 +1,39 @@
 package bsu.rfe.java.group7.lab2.Ramnenok.var8;
 
-public class Main {
+import java.awt.BorderLayout;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JOptionPane;
+import javax.swing.event.MenuEvent;
+import javax.swing.event.MenuListener;
 
-    public static void main(String[] args) {
-	// write your code here
-    }
+public class Main extends JFrame {
+    // Начальные размеры окна приложения
+    private static final int WIDTH = 800;
+    private static final int HEIGHT = 600;
+    // Объект диалогового окна для выбора файлов
+    private JFileChooser fileChooser = null;
+
+    // Пункты меню
+    private JCheckBoxMenuItem showAxisMenuItem;
+    private JCheckBoxMenuItem showMarkersMenuItem;
+    private JCheckBoxMenuItem showAxisSegmentsMenuItem;
+
+    // Компонент-отображатель графика
+    private GraphicsDisplay display = new GraphicsDisplay();
+
+    // Флаг, указывающий на загруженность данных графика
+    private boolean fileLoaded = false;
 }
